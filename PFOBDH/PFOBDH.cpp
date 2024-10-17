ac95d2b53c214a1aeae8577b5d02bce9b4de0000
AlexanderBulow <alexandercarlaxelbulow@gmail.com>
2024-10-17 15:52:11 +0200

    Add project files.

---
+++ PFOBDH/PFOBDH.cpp
@@ -0,0 +1,14 @@
+﻿// PFOBDH.cpp : Defines the entry point for the application.
+//
+
+#include "PFOBDH.h"
+#include "GetCommand.h"
+#include "SendCommand.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
