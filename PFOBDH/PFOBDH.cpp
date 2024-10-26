3a5b471b355ce5f87f0694a111c1120d582ce7a3
ludado1 <ludado-1@student.ltu.se>
2024-10-26 15:02:45 +0200

    some changes

--- PFOBDH/PFOBDH.cpp
+++ PFOBDH/PFOBDH.cpp
@@ -25,31 +25,8 @@ using namespace std;
 
 int main()
 {
-	//while (true) {
-		GetTime();
-
-		SetTime(22,5,0);
-	
-
-		//char command[64] = "something";
-		//SendCom(command, 1);
-
-		//cout << "send command";
-		//char* receiving = GetCommand(1);
-		//cout << receiving << endl;
-
-		
-		cout << "start" << endl;
-		string newmode = "TargetMode";
-		if (SetMode(newmode) == 1) {
-			cout << "The mode has been changed to: " << newmode << endl;
-		}
-		else {
-			cout << "New mode is invalid and therefore mode has not been changed ";
-		}
-		
-		cout << "current mode is " << CheckMode() << endl;
-		
-	//}
+	while (1) {
+		cout << "penis";
+	}
 	return 0;
 }
--- PFOBDH/SetTime.cpp
+++ PFOBDH/SetTime.cpp
@@ -1,7 +1,9 @@
 #include "SetTime.h"
 #include "Init.h"
+#include "GetTime.h"
 using namespace std;
 
 void SetTime(int hours, int minutes, int seconds) {
 	OffsetInTime = hours * 3600 + minutes * 60 + seconds - InitialHoursAsInt*3600 - 60* InitialMinutesAsInt - InitialSecondsAsInt;
+	GetTime();
 }
